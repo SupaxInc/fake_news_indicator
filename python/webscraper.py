febffd040fe08b2dc011aa1dad817071dc9cde90
Paul John Gonzales <paulj_gonzales@hotmail.com>
2021-08-07 00:19:48 -0400

    Creating python class for PubMed objects

--- python/webscraper.py
+++ python/webscraper.py
@@ -1,3 +1,4 @@
+from collections import UserList
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -6,11 +7,45 @@ import struct
 import nativemessaging
 import json
 
+class PubMed:
+
+    # To do: Make construction function more general instead of just pubmed in the case of other databases implemented
+    def __init__(self, searchTerm='', maxSearch='200'):
+        # searchTerm will be sent from chrome extension where white spaces and nulls have been validated
+        searchTerm = searchTerm.replace(" ", "+")
+        url = "https://pubmed.ncbi.nlm.nih.gov/?term=%s&=%s"%(searchTerm, maxSearch)
+        
+        self.url = url
+        self.searchTerm = searchTerm
+        self.maxSearch = maxSearch
+        self.soup = BeautifulSoup(requests.get(url).content, 'html5lib')
+
+    def getMainSearchHTML(self):
+        return self.soup
+
+    def cycleThroughEachResultHTML(self):
+        listUrlID = []
+        pubMedResultURL = self.soup.findAll('a', attrs = {'class': 'docsum-title'})
+
+        for id in pubMedResultURL:
+            listUrlID.append(id.replace("/",""))
+
+
+
+        
+
+
+
 pubMedURL = "https://pubmed.ncbi.nlm.nih.gov/33301246/"
+# get HTML code
 pubMedSoup = BeautifulSoup(requests.get(pubMedURL).content, 'html5lib')
+# find div with full text links
 pubMedFullTextLinks = pubMedSoup.find('div', attrs = {'class':'full-text-links-list'})
+# find a tag element from full text
 pmcElement = pubMedSoup.find('a', attrs = {'class':'link-item pmc'})
+# get the URL of the full text page
 pubMedFullTextURL = pmcElement["href"]
+# load the HTML of the full text link
 pubMedFullTextSoup = BeautifulSoup(requests.get(pubMedFullTextURL).content, 'html5lib')
 #print(pubMedFullTextSoup.prettify())
 #print(pubMedFullTextSoup.select("p"))
@@ -19,3 +54,7 @@ while True:
     message = nativemessaging.get_message()
     if message == "hello":
         nativemessaging.send_message(nativemessaging.encode_message("world"))
+
+
+
+
--- scripts/popup_script.js
+++ scripts/popup_script.js
@@ -1,63 +1,45 @@
-let querying = chrome.tabs.query({active: true, currentWindow: true}); // Chrome query that grabs the active tab object for the current window
+// Chrome query that grabs the active tab object for the current window
+let querying = chrome.tabs.query({active: true, currentWindow: true}); 
 querying.then(sendHighlightedText, onError);
 
-
-// runtime.connectNative
+// Open the port to python app
 var port = chrome.runtime.connectNative("com.pysearch.bat");
-// Receives the message that was sent from the python script
+// Listener that checks if extension has received the message that was sent from the python script
 port.onMessage.addListener(handleMessages);
-port.postMessage("hello");
-
-
-// runtime.sendNativeMessage
-//browser.runtime.sendNativeMessage("application_name", "hello").then(handleMessages);
 
 
 $(document).ready(function () {
+
+    // User pressing the search button
     $("#search_btn").on("click", function () {
         let criteriaVal = $("#criteria").val();
 
-        // Check if the search bar is empty or not
-        if (criteriaVal != "") {
+        // Check if the search bar is NOT empty or has white spaces
+        if (!isEmptyOrSpaces(criteriaVal)) {
             port.postMessage("hello");
-            // port.postMessage({
-            //     "message": "get_msg",
-            //     "payload": criteriaVal
-            // });
         }
     });
 });
 
+function isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
 
 
 
 // Handles messages sent by the python script
 function handleMessages(response) {
     console.log(response);
-    // if(request.message ===  "get_txt") {
-    //     if (chrome.runtime.lastError) {
-    //         sendResponse({
-    //             message: "fail"
-    //         });
-    //         return;
-    //     }
-
-    //     sendResponse({
-    //         message: "successful",
-    //         payload: getHighlightedText()
-    //     });
-    //     // Need to return true if using async code to keep communication line open.
-    //     return true;
-    // }
 }
 
 
 // Sends highlighted text to pop up script
 function sendHighlightedText(tabs) {
-    console.log(tabs[0].id);
+    // Send message to the content script
     chrome.tabs.sendMessage(tabs[0].id, {
         message: "get_txt"
     }, response => {
+        // Check if the response from pop up script is "successfull"
         if (response.message === "successful") {
             $("#criteria").val(response.payload);
         }
